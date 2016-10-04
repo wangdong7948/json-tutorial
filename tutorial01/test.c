cd7807fbe3a71e13d3e7c27ef7aac7306d65f9ce
wangdong7948 <wangdong_27@163.com>
2016-10-04 09:46:48 +0800

    Update test.c

--- tutorial01/test.c
+++ tutorial01/test.c
@@ -27,6 +27,8 @@ static void test_parse_null() {
     EXPECT_EQ_INT(LEPT_NULL, lept_get_type(&v));
 }
 
+
+
 static void test_parse_expect_value() {
     lept_value v;
 
@@ -51,10 +53,9 @@ static void test_parse_invalid_value() {
 }
 
 static void test_parse_root_not_singular() {
-    lept_value v;
-    v.type = LEPT_FALSE;
-    EXPECT_EQ_INT(LEPT_PARSE_ROOT_NOT_SINGULAR, lept_parse(&v, "null x"));
-    EXPECT_EQ_INT(LEPT_NULL, lept_get_type(&v));
+   if(json!=null && json++ =null && json++ +1!=null)          
+    return LEPT_PARSE_ROOT_NOT_SINGULAR
+    
 }
 
 static void test_parse() {
